fda15ad51c84b2e9e14c75ef6e4abece8bf0d665
Abdulrahman Bello <baffagry44@gmail.com>
2020-11-18 22:35:44 +0100

    upload lessons details to lesson.js

---
+++ lesson.js
@@ -0,0 +1,98 @@
+let lessons = [
+    {
+        "id": 1001,
+        "subject": 'Science',
+        "location": "Dubai",
+        "price": 30,
+        "image": "https://miro.medium.com/max/1400/1*Nvhe7mchQmJBgyVx-uOBrA.png",
+        "spaces": 5
+    },
+    {
+        "id": 1002,
+        "subject": 'Health',
+        "location": "Abu Dhabi",
+        "price": 50,
+        "image": "https://houstonhealthcareinitiative.org/wp-content/uploads/2020/04/2017-12-12-patient-care.png",
+        "spaces":5
+    },
+    {
+        "id": 1003,
+        "subject": 'Physical Education',
+        "location": "Deira",
+        "price": 25,
+        "image": "https://www.pittsfordschools.org/cms/lib/NY02205365/Centricity/Domain/1051/900a3e5531049d1201d8a550080ae59f.png",
+        "spaces":5
+    },
+    {
+        "id": 1004,
+        "subject": 'Art',
+        "location": "Sharjah",
+        "price": 15,
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/440px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg",
+        "spaces": 5
+    },
+    {
+        "id": 1005,
+        "subject": 'Music',
+        "location": "Ajman",
+        "price": 12,
+        "image": "https://www.ncw.co.uk/wp-content/uploads/2020/06/Music-Club.jpg",
+        "spaces":5
+    },
+    {
+        "id": 1006,
+        "subject": 'Dance',
+        "location": "Fujaira",
+        "price": 35,
+        "image": "https://ket-uploads-education-ga.s3.amazonaws.com/wp-content/uploads/3616.jpg",
+        "spaces":5
+    },
+    {
+        "id": 1007,
+        "subject": 'Special Education',
+        "location": "Al Ain",
+        "price": 19,
+        "image": "http://charlottesvilleschools.org/wp-content/uploads/2017/05/special-needs-SEAC-768x650.jpg",
+        "spaces": 5
+    },
+    {
+        "id": 1008,
+        "subject": 'English',
+        "location": "Ras Al Khaima",
+        "price": 20,
+        "image": "https://img.jakpost.net/c/2020/05/05/2020_05_05_94481_1588657712._large.jpg",
+        "spaces":5
+    },
+    {
+        "id": 1009,
+        "subject": 'Math',
+        "location": "Jebel Ali",
+        "price": 19,
+        "image": "https://image.freepik.com/free-vector/math-background_23-2148146270.jpg",
+        "spaces":5
+    },
+    {
+        "id": 1010,
+        "subject": 'Life Science',
+        "location": "Kalba",
+        "price": 20,
+        "image": "https://www.healtheuropa.eu/wp-content/uploads/2019/09/iStock-DrAfter123-1068x601.jpg",
+        "spaces": 5
+    },
+    {
+        "id": 1011,
+        "subject": 'Physical Science',
+        "location": "Hatta",
+        "price": 13,
+        "image": "https://www.gostudy.net/img/9397f8c3-955a-42ae-ba56-a06d6af88bac/featured/ahi/physical-sciences.jpg",
+        "spaces":5
+    },
+    {
+        "id": 1012,
+        "subject": 'Business Technology',
+        "location": "Al Madam",
+        "price": 25,
+        "image": "http://www.techconnections.org/wp-content/uploads/2018/10/tech-for-biz.jpg",
+        "spaces":5
+    }
+]
